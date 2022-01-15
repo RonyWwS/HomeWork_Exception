a7da9f16b0a2e0bab6fcf0cb8072601fb819bc1f
Denis <1997deny@gmail.com>
2022-01-15 17:12:47 +0500

    HomeWork Exception

--- src/main/java/HomeWork_Exception/main.java
+++ src/main/java/HomeWork_Exception/main.java
@@ -7,21 +7,36 @@ public class main {
         String[][] stringArray = new String[][] {{"1","2","3","4"}, {"word","word","word","word"}, {"5","6","7","8"},{"word","word","word","word"}};
 
         try {
-            stringArrayException(stringArray);
-        } catch (MyArraySizeException e) {
-            e.printStackTrace();
+            try {
+                int result = stringArrayException(stringArray);
+                System.out.println("Result = " + result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Array size > 4");
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("Wrong data array");
+            System.out.println("Coordinate error: y " + e.i + " x " + e.j);
         }
     }
 
-    private static void stringArrayException (String[][] stringArray) throws MyArraySizeException {
-        if (stringArray.length != 4) {
+    private static int stringArrayException (String[][] stringArray) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if(stringArray.length != 4) {
             throw new MyArraySizeException();
-        } else {
-            for (int i = 0; i < stringArray.length; i++) {
-                for (int j = 0; j < stringArray.length; j++) {
-                    System.out.println(stringArray);
+        }
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < stringArray[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(stringArray[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
                 }
             }
         }
+        return count;
     }
 }
